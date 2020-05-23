70b96298451fdee5062e16857b050cda40aa9c33
leixiang <lynnl.yet@gmail.com>
2020-05-23 08:40:49 +0800

    Refine __assert_cmp*(); Add BUILD_BUG_ON() macro

--- assertf.h
+++ assertf.h
@@ -1,5 +1,5 @@
 /*
- * Out-of-box format-able assert macros
+ * Header-only format-able assert macros
  *
  * Created May 21, 2020. leiless.
  * XXX: side-effect unsafe
@@ -15,6 +15,23 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <libgen.h>
+
+/**
+ * Compile-time assertion  see: linux/arch/x86/boot/boot.h
+ *
+ * [sic modified]
+ * BUILD_BUG_ON - break compile if a condition is true.
+ * @cond: the condition which the compiler should know is false.
+ *
+ * If you have some code which relies on certain constants being equal, or
+ * some other compile-time-evaluated condition, you should use BUILD_BUG_ON to
+ * detect if someone changes it.
+ */
+#ifdef DEBUG
+#define BUILD_BUG_ON(cond)      ((void) sizeof(char[1 - 2*!!(cond)]))
+#else
+#define BUILD_BUG_ON(cond)      ((void) (cond))
+#endif
 
 /* Macro taken from macOS/Frameworks/Kernel/sys/cdefs.h */
 #ifndef __printflike
@@ -33,10 +50,10 @@ void __assertf0(int, const char *, ...) __printflike(2, 3);
  */
 void __assertf0(int expr, const char *fmt, ...)
 {
+#ifndef NO_ASSERTF_H
     int n;
     va_list ap;
 
-#ifndef NO_ASSERTF_H
     if (!expr) {
         va_start(ap, fmt);
         n = vfprintf(stderr, fmt, ap);
@@ -45,6 +62,10 @@ void __assertf0(int expr, const char *fmt, ...)
 
         abort();
     }
+#else
+    /* Supress unused-variable warnings */
+    (void) expr;
+    (void) fmt;
 #endif
 }
 
@@ -56,6 +77,10 @@ void __assertf0(int expr, const char *fmt, ...)
 #define assert_nonnull(ptr)         assertf(ptr != NULL, "")
 #define assert_null(ptr)            assertf(ptr == NULL, "")
 
+#define __assert_cmp0(a, b, fs, op)                         \
+    assertf((a) op (b), "lhs: " fs " rhs: " fs,             \
+            (a), (typeof(a)) (b))
+
 /**
  * @param a     Left hand side
  * @param b     Right hand side
@@ -64,27 +89,27 @@ void __assertf0(int expr, const char *fmt, ...)
  * @param fmt   Additional format string
  * @param ...   Format string arguments
  */
-#define __assert_cmp(a, b, fs, op, fmt, ...)                \
-    assertf((a) op (b), "lhs: " fs " rhs: " fs " " fmt,     \
+#define __assert_cmp1(a, b, fs, op, fmt, ...)               \
+    assertf((a) op (b), "lhs: " fs " rhs: " fs " - " fmt,   \
             (a), (typeof(a)) (b), ##__VA_ARGS__)
 
-#define assert_eq(a, b, fs)             __assert_cmp(a, b, fs, ==, "")
-#define assert_eqf(a, b, fs, fmt, ...)  __assert_cmp(a, b, fs, ==, fmt, ##__VA_ARGS__)
+#define assert_eq(a, b, fs)             __assert_cmp0(a, b, fs, ==)
+#define assert_eqf(a, b, fs, fmt, ...)  __assert_cmp1(a, b, fs, ==, fmt, ##__VA_ARGS__)
 
-#define assert_ne(a, b, fs)             __assert_cmp(a, b, fs, !=, "")
-#define assert_nef(a, b, fs, fmt, ...)  __assert_cmp(a, b, fs, !=, fmt, ##__VA_ARGS__)
+#define assert_ne(a, b, fs)             __assert_cmp0(a, b, fs, !=)
+#define assert_nef(a, b, fs, fmt, ...)  __assert_cmp1(a, b, fs, !=, fmt, ##__VA_ARGS__)
 
-#define assert_le(a, b, fs)             __assert_cmp(a, b, fs, <=, "")
-#define assert_lef(a, b, fs, fmt, ...)  __assert_cmp(a, b, fs, <=, fmt, ##__VA_ARGS__)
+#define assert_le(a, b, fs)             __assert_cmp0(a, b, fs, <=)
+#define assert_lef(a, b, fs, fmt, ...)  __assert_cmp1(a, b, fs, <=, fmt, ##__VA_ARGS__)
 
-#define assert_ge(a, b, fs)             __assert_cmp(a, b, fs, >=, "")
-#define assert_gef(a, b, fs, fmt, ...)  __assert_cmp(a, b, fs, >=, fmt, ##__VA_ARGS__)
+#define assert_ge(a, b, fs)             __assert_cmp0(a, b, fs, >=)
+#define assert_gef(a, b, fs, fmt, ...)  __assert_cmp1(a, b, fs, >=, fmt, ##__VA_ARGS__)
 
-#define assert_lt(a, b, fs)             __assert_cmp(a, b, fs, <, "")
-#define assert_ltf(a, b, fs, fmt, ...)  __assert_cmp(a, b, fs, <, fmt, ##__VA_ARGS__)
+#define assert_lt(a, b, fs)             __assert_cmp0(a, b, fs, <)
+#define assert_ltf(a, b, fs, fmt, ...)  __assert_cmp1(a, b, fs, <, fmt, ##__VA_ARGS__)
 
-#define assert_gt(a, b, fs)             __assert_cmp(a, b, fs, >, "")
-#define assert_gtf(a, b, fs, fmt, ...)  __assert_cmp(a, b, fs, >, fmt, ##__VA_ARGS__)
+#define assert_gt(a, b, fs)             __assert_cmp0(a, b, fs, >)
+#define assert_gtf(a, b, fs, fmt, ...)  __assert_cmp1(a, b, fs, >, fmt, ##__VA_ARGS__)
 
 #endif
 
